367d8b9e3dc2a2707e53a54c6ffb8408d2ba399b
Lakulength@1717 <girm2002@gmail.com>
2025-03-10 18:36:33 -0400

    nav modified for render

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav: React.FC = () => {
       <div className="nav-container">
         {/* Home link now points to an external URL */}
         <div className="nav-item">
-          <a href="http://localhost:5173/" className="nav-link" target="_blank" rel="noopener noreferrer">
+          <a href="https://challenge13-fypj.onrender.com/" className="nav-link" target="_blank" rel="noopener noreferrer">
             Home
           </a>
         </div>
